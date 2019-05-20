854dc8b9e1c3c8b49562e55e9c7f3b9a640eb242
Leandro Mancini de Sousa <leandro.sousa@bgmrodotec.com.br>
2019-05-20 00:05:58 -0300

    Layout valetas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  selectedIndex = 0;
+
+  selectedTabChange(evt: MatTabChangeEvent) {
+    console.log(evt.index);
+
+    this.selectedIndex = evt.index;
+  }
 }
